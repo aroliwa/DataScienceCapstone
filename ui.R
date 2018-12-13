eaac0d99797cf9184676b692fbac33978c185a57
aroliwa <aroliwa@gmail.com>
2018-12-13 09:09:40 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -15,12 +15,14 @@ dashboardPage(
       tabItem(tabName = "text",
               h2("Coursera Data Science Capstone Project"),
               tags$img(src = "logocoursera.png", height=100, width =250),
-              
+              hr(),
               textInput("text", label = h2("Enter text..."), value = ""),
               
-              hr(),
+              
+              tags$small("(The app runs slightly slow, so please wait a moment for the prediction to appear)"),
               p("The predicted next word:"),
-              fluidRow(column(3, verbatimTextOutput("value") ))
+              
+              fluidRow(column(3, withSpinner(verbatimTextOutput("value"), 5) ))
               
       ),
       
